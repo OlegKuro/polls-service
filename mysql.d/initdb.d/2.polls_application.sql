962debd0571c89c9b36ed4f2c5610b847da86c18
ohayatov <ohayatov@webim.ru>
2018-05-09 22:30:54 +0300

    DB structure final version

--- mysql.d/initdb.d/2.polls_application.sql
+++ mysql.d/initdb.d/2.polls_application.sql
@@ -3,6 +3,16 @@ drop database if exists polls_application;
 create database polls_application;
 
 use polls_application;
+DROP TABLE IF EXISTS `UserData`;
+
+CREATE TABLE `UserData` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `surname` VARCHAR(255) NOT NULL,
+  `country` VARCHAR(255) NOT NULL,
+  `city` VARCHAR(255) NOT NULL,
+  `birth_date` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 DROP TABLE IF EXISTS `Polls`;
 
@@ -10,17 +20,21 @@ CREATE TABLE `Polls` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `creation` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`)
+    REFERENCES UserData(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `Variants`;
 
 CREATE TABLE `Variants` (
   `poll_id` INT NOT NULL,
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
   `option` VARCHAR(255) NOT NULL,
-  `votes_count` INT DEAULT 0,
-  PRIMARY KEY (`poll_id, id`)
+  `votes_count` INT DEFAULT 0,
+  PRIMARY KEY (`poll_id`, `id`),
+  FOREIGN KEY (`poll_id`)
+    REFERENCES Polls(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `Votes`;
@@ -29,16 +43,9 @@ CREATE TABLE `Votes` (
   `poll_id` INT NOT NULL,
   `variant_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  PRIMARY KEY (`poll_id`, `variant_id`, `user_id`)
+  PRIMARY KEY (`poll_id`, `user_id`),
+  FOREIGN KEY (`poll_id`)
+    REFERENCES Polls(`id`),
+  FOREIGN KEY (`user_id`)
+    REFERENCES UserData(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `User`;
-
-CREATE TABLE `User` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `surname` VARCHAR(255) NOT NULL,
-  `country` VARCHAR(255) NOT NULL,
-  `city` VARCHAR(255) NOT NULL,
-  `birth_date` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-)
